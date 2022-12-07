6fed023dc17d7d76473e8b0111887d57dd4ba9d3
Florian König <koenidv@gmail.com>
2022-12-07 17:51:36 +0100

    Use space for command invocation

--- src/hooks/parseInputCommand.ts
+++ src/hooks/parseInputCommand.ts
@@ -1,6 +1,6 @@
 export const parseInputCommand = (inputValue: string) => {
   const [match, keyword, query] =
-    inputValue.match(/^([a-zA-Z]{1,2})>(.*)/) || [];
+    inputValue.match(/^([a-zA-Z]{1,2})\s(.*)/) || [];
   return {
     didMatch: match !== undefined,
     keyword: keyword?.toLowerCase() || "",
